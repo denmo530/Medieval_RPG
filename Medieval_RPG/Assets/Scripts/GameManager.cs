badfbdc831e791c4a4857ba14a8795b0b0d29c27
timol085 <timol085@student.liu.se>
2022-09-21 16:17:15 +0200

    Load and Save + start at text system

---
+++ Medieval_RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+    }
+    //Ressources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSrites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+
+    //References
+    public Player player;
+    //Public weapon weapon..
+
+    //Logic
+    public int pesos;
+    public int experience;
+
+    //Save state
+    /*  
+    INT preferredSkin
+    INT pesos
+    INT experience
+    INT weaponLevel1
+    */
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        PlayerPrefs.SetString("SaveState", s);
+    }
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+        {
+            return;
+        }
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        //Change player skin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //Change the weapon level
+        Debug.Log("LoadState");
+    }
+}
--- Medieval_RPG/Assets/Scripts/Portal.cs
+++ Medieval_RPG/Assets/Scripts/Portal.cs
@@ -4,11 +4,13 @@ public class Portal : Collidable
 {
 
     public string[] sceneNames;
+
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.name == "Player")
         {
             //teleport player
+            GameManager.instance.SaveState();
             string sceneName = sceneNames[0];
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
         }
